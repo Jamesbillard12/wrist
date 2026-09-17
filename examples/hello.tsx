1c619bf562c0eb8cef5f299b67aa988f438c951c
James Billard <Jamesbillard12@hotmail.com>
2026-09-17 13:12:15 -0700

    docs: add first Wrist TSX example

---
+++ examples/hello.tsx
@@ -0,0 +1,10 @@
+export default function Hello() {
+  return (
+    <VStack>
+      <Text>Hello, Wrist!</Text>
+      <HStack>
+        <Text>Native SwiftUI output</Text>
+      </HStack>
+    </VStack>
+  );
+}
